b6f237f210bafae2227d86f8cd93724c4f322817
Jake Mulford <jakemulf@gmail.com>
2021-10-30 11:39:07 -0400

    validate.py review comments

--- validate/validate.py
+++ validate/validate.py
@@ -9,13 +9,14 @@ from pydeephaven import Session, DHError
 
 import time
 
-def main(table_names, host=None):
+def main(table_names: str, host: str, max_retries: int):
     """
     Main method for the script. Simply validates that each table exists
 
     Parameters:
         table_names (list<str>): A list of table names to validate
-        host (str): An optional host if not using the default
+        host (str): The host name of the Deephaven instance
+        max_retries (int): The maximum attempts to retry connecting to Deephaven
 
     Returns:
         None
@@ -28,39 +29,40 @@ def main(table_names, host=None):
 
     #Simple retry loop in case the server tries to launch before Deephaven is ready
     count = 0
-    max_count = 5
-    while (count < max_count):
+    while (count < max_retries):
         try:
-            if host is None:
-                session = Session()
-            else:
-                session = Session(host=host)
-            count = max_count
-        except:
+            session = Session(host=host)
+            continue
+        except DHError as e:
             print("Failed to connect to Deephaven... Waiting to try again")
+            print(e)
             time.sleep(5)
             count += 1
+        except Exception as e:
+            print("Unknown error when connecting to Deephaven")
+            print(e)
+            sys.exit()
     if session is None:
-        sys.exit("Failed to connect to Deephaven after 5 attempts")
+        sys.exit("Failed to connect to Deephaven after %d attempts" % max_retries)
 
     table = session.empty_table(3)
     for table_name in table_names:
         try:
             #session.open_table(table_name)
             #Temporary workaround: This script is sufficient to check that the table exists
-            session.run_script("{table}={table}".format(table=table_name))
-            print("Validates presence of table {table}".format(table=table_name))
+            session.run_script("%s=%s" % (table_name, table_name))
+            print("Table is present: %s" % table_name)
         except DHError as e:
-            print("Deephaven error when trying to open table {table_name}".format(table_name=table_name))
+            print("Deephaven error when trying to access table: %s" % table_name)
             print(e)
             exit(1)
         except Exception as e:
-            print("Unexpected error when trying to open table {table_name}".format(table_name=table_name))
+            print("Unexpected error when trying to access table: %s" % table_name)
             print(e)
             exit(1)
 
 usage = """
-usage: python validate.py <table-names> host
+usage: python validate.py table-names host
 """
 
 if __name__ == '__main__':
@@ -70,12 +72,13 @@ if __name__ == '__main__':
         #double quotes, so it's like '"value,value"'. This splice removes those quotes
         #before splitting on the comma
         table_names = sys.argv[1][1:-1].split(",")
+        host = sys.argv[2][1:-1]
     except:
         print(usage)
         exit(1)
 
-    host = None
     if len(sys.argv) > 2:
-        host = sys.argv[2][1:-1]
+        print(usage)
+        exit(1)
 
     main(table_names, host)
